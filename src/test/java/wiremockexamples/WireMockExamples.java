d697aea5dc0c8bc1cd7742572a21b6a969ebd76e
Bas Dijkstra <bas@ontestautomation.com>
2019-10-16 16:03:39 +0200

    Added examples, exercises and answers for stateful mocks

--- src/test/java/wiremockexamples/WireMockExamples.java
+++ src/test/java/wiremockexamples/WireMockExamples.java
@@ -1,6 +1,7 @@
 package wiremockexamples;
 
 import com.github.tomakehurst.wiremock.http.Fault;
+import com.github.tomakehurst.wiremock.stubbing.Scenario;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
@@ -37,7 +38,6 @@ public class WireMockExamples {
             ));
     }
 
-
     public void setupStubReturningErrorCode() {
 
         stubFor(get(urlEqualTo("/errorcode"))
@@ -61,5 +61,31 @@ public class WireMockExamples {
             .willReturn(aResponse()
                 .withFault(Fault.MALFORMED_RESPONSE_CHUNK)
             ));
+    }
+
+    public void setupStubStateful() {
+
+        stubFor(get(urlEqualTo("/order")).inScenario("Order processing")
+            .whenScenarioStateIs(Scenario.STARTED)
+            .willReturn(aResponse()
+                .withBody("Your shopping cart is empty")
+            )
+        );
+
+        stubFor(post(urlEqualTo("/order")).inScenario("Order processing")
+            .whenScenarioStateIs(Scenario.STARTED)
+            .withRequestBody(equalTo("Ordering 1 item"))
+            .willReturn(aResponse()
+                .withBody("Item placed in shopping cart")
+            )
+            .willSetStateTo("ORDER_PLACED")
+        );
+
+        stubFor(get(urlEqualTo("/order")).inScenario("Order processing")
+            .whenScenarioStateIs("ORDER_PLACED")
+            .willReturn(aResponse()
+                .withBody("There is 1 item in your shopping cart")
+            )
+        );
     }
 }
