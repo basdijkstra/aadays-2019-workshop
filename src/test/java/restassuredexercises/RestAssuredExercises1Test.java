10efeac3c9b0983f4b332e064d6fa8bf2e49c40d
Bas Dijkstra <bas@ontestautomation.com>
2019-10-25 09:26:31 +0200

    Changed comments

--- src/test/java/restassuredexercises/RestAssuredExercises1Test.java
+++ src/test/java/restassuredexercises/RestAssuredExercises1Test.java
@@ -12,103 +12,103 @@ public class RestAssuredExercises1Test {
 	@Rule
 	public WireMockRule wireMockRule = new WireMockRule(options().port(9876));
 
-	/*******************************************************
-	 * Send a GET request to http://localhost:9876/us/90210
-	 * and check that the response has HTTP status code 200
-	 ******************************************************/
-	
 	@Test
 	public void requestUsZipCode90210_checkResponseCode_expect200() {
+
+		/*******************************************************
+		 * Send a GET request to http://localhost:9876/us/90210
+		 * and check that the response has HTTP status code 200
+		 ******************************************************/
 				
 		given().
 		when().
 		then();
 	}
-	
-	/*******************************************************
-	 * Send a GET request to http://localhost:9876/us/99999
-	 * and check that the answer has HTTP status code 404
-	 ******************************************************/
-	
+
 	@Test
 	public void requestUsZipCode99999_checkResponseCode_expect404() {
-				
+
+		/*******************************************************
+		 * Send a GET request to http://localhost:9876/us/99999
+		 * and check that the answer has HTTP status code 404
+		 ******************************************************/
+
 		given().
 		when().
 		then();
 	}
-	
-	/*******************************************************
-	 * Send a GET request to http://localhost:9876/us/90210
-	 * and check that the response is in JSON format 
-	 ******************************************************/
-	
+
 	@Test
 	public void requestUsZipCode90210_checkContentType_expectApplicationJson() {
+
+		/*******************************************************
+		 * Send a GET request to http://localhost:9876/us/90210
+		 * and check that the response is in JSON format
+		 ******************************************************/
 				
 		given().
 		when().
 		then();
 	}
-	
-	/***********************************************
-	 * Send a GET request to http://localhost:9876/us/90210
-	 * and check that the state associated with the first
-	 * place in the list returned is equal to 'California'
-	 * Use the GPath expression places[0].state to retrieve
-	 * the value for the state
-	 **********************************************/
-	
+
 	@Test
 	public void requestUsZipCode90210_checkStateForFirstPlace_expectCalifornia() {
+
+		/***********************************************
+		 * Send a GET request to http://localhost:9876/us/90210
+		 * and check that the state associated with the first
+		 * place in the list returned is equal to 'California'
+		 * Use the GPath expression places[0].state to retrieve
+		 * the value for the state
+		 **********************************************/
 				
 		given().
 		when().
 		then();
 	}
-	
-	/***********************************************
-	 * Send a GET request to http://localhost:9876/de/24848
-	 * and check that the list of place names returned
-	 * contains the value 'Kropp'.
-	 * Use the GPath expression places.'place name' to retrieve
-	 * the list of place names.
-	 **********************************************/
-	
+
 	@Test
 	public void requestDeZipCode24848_checkListOfPlaceNames_expectContainsKropp() {
+
+		/***********************************************
+		 * Send a GET request to http://localhost:9876/de/24848
+		 * and check that the list of place names returned
+		 * contains the value 'Kropp'.
+		 * Use the GPath expression places.'place name' to retrieve
+		 * the list of place names.
+		 **********************************************/
 		
 		given().
 		when().
 		then();
 	}
 
-	/***********************************************
-	 * Send a GET request to http://localhost:9876/de/24848
-	 * and check that the list of place names returned does not
-	 * contain the value 'Frankfurt'.
-	 * Use the GPath expression places.'place name' to retrieve
-	 * the list of place names.
-	 **********************************************/
-	
 	@Test
 	public void requestDeZipCode24848_checkListOfPlaceNames_expectDoesNotContainFrankfurt() {
+
+		/***********************************************
+		 * Send a GET request to http://localhost:9876/de/24848
+		 * and check that the list of place names returned does not
+		 * contain the value 'Frankfurt'.
+		 * Use the GPath expression places.'place name' to retrieve
+		 * the list of place names.
+		 **********************************************/
 		
 		given().
 		when().
 		then();
 	}
-
-	/***********************************************
-	 * Send a GET request to http://localhost:9876/de/24848
-	 * and check that the list of place names returned is a
-	 * collection of size 4.
-	 * Use the GPath expression places.'place name' to retrieve
-	 * the list of place names.
-	 **********************************************/
 
 	@Test
 	public void requestDeZipCode24848_checkNumberOfPlaceNames_expect4() {
+
+		/***********************************************
+		 * Send a GET request to http://localhost:9876/de/24848
+		 * and check that the list of place names returned is a
+		 * collection of size 4.
+		 * Use the GPath expression places.'place name' to retrieve
+		 * the list of place names.
+		 **********************************************/
 
 		given().
 		when().
--- src/test/java/restassuredexercises/RestAssuredExercises2Test.java
+++ src/test/java/restassuredexercises/RestAssuredExercises2Test.java
@@ -18,34 +18,35 @@ public class RestAssuredExercises2Test {
 	@Rule
 	public WireMockRule wireMockRule = new WireMockRule(options().port(9876));
 
-	/*******************************************************
-	 * Have this DataProvider return three test data rows:
-	 * country code - zip code - state
-	 * us           - 90210    - California
-	 * us           - 12345    - New York
-	 * ca           - Y1A      - Yukon
-	 ******************************************************/
-	
 	@DataProvider
 	public static Object[][] zipCodeData() {
+
+		/*******************************************************
+		 * Have this DataProvider return three test data rows:
+		 * country code - zip code - state
+		 * us           - 90210    - California
+		 * us           - 12345    - New York
+		 * ca           - Y1A      - Yukon
+		 ******************************************************/
+
 		return new Object[][] {
 			// TEST DATA GOES HERE
 		};
 	}
-	
-	/*******************************************************
-	 * Request zip code data for given country / zip
-	 * combinations by sending a GET to http://localhost:9876/{countryCode}/{zipCode}.
-	 * Use the test data collection created
-	 * above. Check that the state returned by the API
-	 * matches the expected value.
-	 * Use the GPath expression places[0].state to retrieve
-	 * the value for the state.
-	 ******************************************************/
-	
+
 	@Test
 	@UseDataProvider("zipCodeData")
 	public void checkStateForCountryCodeAndZipCode() {
+
+		/*******************************************************
+		 * Request zip code data for given country / zip
+		 * combinations by sending a GET to http://localhost:9876/{countryCode}/{zipCode}.
+		 * Use the test data collection created
+		 * above. Check that the state returned by the API
+		 * matches the expected value.
+		 * Use the GPath expression places[0].state to retrieve
+		 * the value for the state.
+		 ******************************************************/
 		
 		given().
 		when().
